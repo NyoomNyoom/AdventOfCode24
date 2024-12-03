4432402c549152ba8cc8146d8d9e6929127ab2df
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-12-03 21:53:06 +1300

    Solving part 1

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,10 +1,24 @@
+import math
+
 testFile = open("c:/coding-projects/AdventOfCode24/Day 1/day1.txt", "r")
 
 leftList = []
 rightList = []
+total = 0
+i = 0
 
 for line in testFile:
     splitLine = line.split()
     leftList.append(int(splitLine[0]))
     rightList.append(int(splitLine[1]))
 
+leftList.sort()
+rightList.sort()
+
+
+
+for num in leftList:
+    total += abs(num - rightList[i])
+    i += 1
+
+print(total)
